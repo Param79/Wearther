49c40bbd55bff21b65ff3f0f80b2c179d5aa6f0c
Param79 <152066556+Param79@users.noreply.github.com>
2023-12-26 21:31:31 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+// Optional: close the dropdown menu when clicking outside of it
+//document.addEventListener("click", function(event) {
+   // var dropdowns = document.getElementsByClassName("dropdown-menu");
+   // for (var i = 0; i < dropdowns.length; i++) {
+  //    var dropdown = dropdowns[i];
+  //    if (!dropdown.contains(event.target)) {
+    //    dropdown.style.display = "none";
+  //    }
+ //   }
+ // });//
+  
